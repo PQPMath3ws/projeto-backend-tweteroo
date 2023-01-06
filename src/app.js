0c25b0c6c9c07c103a1b83c4daaf7c853dac0134
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-06 09:46:15 -0300

    Importação manual das rotas da aplicação

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import cors from "cors";
 import express from "express";
 
+import SignUp from "./routes/sign-up.js";
+import Tweets from "./routes/tweets.js";
+import Errors from "./routes/errors.js";
+
 const server = express();
 const PORT = 5000;
 
 server.use(cors());
+
+server.use(SignUp);
+server.use(Tweets);
+server.use(Errors);
 
 server.listen(PORT);
