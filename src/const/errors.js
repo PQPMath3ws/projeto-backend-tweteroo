8a533676a15278f7f01a2eb66849c86df77a5048
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-07 00:31:17 -0300

    Correção na padronização dos textos dos erros

--- src/const/errors.js
+++ src/const/errors.js
@@ -3,8 +3,8 @@ const setError = (code, message) => ({code, message});
 const errors = {
     404: setError(404, "Rota não encontrada na API do servidor!"),
     405: setError(405, "Método não permitido!"),
-    415: setError(415, "Content-Type Inválido!"),
-    422: setError(422, "Todos os campos são obrigatórios"),
+    415: setError(415, "Content-type inválido!"),
+    422: setError(422, "Todos os campos na requisição são obrigatórios"),
 };
 
 export default errors;
